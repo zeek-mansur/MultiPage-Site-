68a0633512baad50556e28ee920159c698aefbd4
Zeek <mzulkiful@yahoo.com>
2022-09-19 22:01:09 +0100

    React Router and Pages Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <nav>
         <h1>My Articles</h1>
       </nav>
+
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      </BrowserRouter>
     </div>
+
   );
 }
 
