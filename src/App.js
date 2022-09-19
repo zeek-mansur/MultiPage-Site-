be840b7a57e55013f79a2c84a8c2aad93d16d204
Zeek <mzulkiful@yahoo.com>
2022-09-19 18:50:38 +0100

    Multipage React site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>My Articles</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
