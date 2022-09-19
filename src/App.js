9b57140ee1040c7de0b5a1d902b4716534e29619
Zeek <mzulkiful@yahoo.com>
2022-09-19 22:08:55 +0100

    Switch and Exact Matches

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -13,7 +13,8 @@ function App() {
         <h1>My Articles</h1>
       </nav>
 
-      <Route path="/">
+     <Switch>
+     <Route exact path="/">
         <Home />
       </Route>
       <Route path="/about">
@@ -22,6 +23,7 @@ function App() {
       <Route path="/contact">
         <Contact />
       </Route>
+     </Switch>
       </BrowserRouter>
     </div>
 
