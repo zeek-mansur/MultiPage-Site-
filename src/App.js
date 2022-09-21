471894d4ed234e2f529563e312d32cb27c5d9894
Zeek <mzulkiful@yahoo.com>
2022-09-21 21:58:38 +0100

    The useParams Hook

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
      <Route path="/articles/:id">
             <Article />    
       </Route>
-      
+
       </BrowserRouter>
      
     </div>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 
 export default function Article() {
+
+  const {id} = useParams()
+  const url = 'http://localhost:3000/articles/' + id
+  const {data: article, isPending, error} = useFetch(url)
+
   return (
     <div>
-      Article pages
+     {isPending && <div> Loading...</div>}
+     {error && <div>{error}</div> }
+     {article && (
+      <div>
+        <h2>{article.title}</h2>
+        <p>By {article.author}</p>
+        <p>{article.body}</p>
+      </div>
+     )}
     </div>
   )
 }
